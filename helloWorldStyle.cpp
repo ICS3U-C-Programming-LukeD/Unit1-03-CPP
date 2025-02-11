ff1d40dbec25f4fc32dec8241a85a2fd47cf27a7
Luke Di Bert <luke.di-bert@stu.ocsb.ca>
2025-02-11 18:04:57 +0000

    added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 Luke Di Bert All rights reserved.
+//
+// Created by: Luke Di Bert
+// Date: Feb 11, 2025
+// Displays a message with style
+
+#include <iostream>
+ int main() {
+    std::cout <<"Hello, I'm using proper style"<< std::endl;
+    std::cout <<"I'm testing this function"<< std::endl;
+}
